0814dc7aaa30225d3c911a8e8a6feeacc67026ba
bbang <89963270+hwldus@users.noreply.github.com>
2022-11-23 16:04:00 +0900

    해결중..

--- src/bubble/game/startPage.java
+++ src/bubble/game/startPage.java
@@ -44,7 +44,7 @@ public class startPage extends JFrame {
         });
     }
 
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
         new startPage();
-    }
+    }*/
 }
